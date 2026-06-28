f8b7e80e44a13c579265ed2a4e4cce648951fafe
Raymond Domanico <r.domanicoo09@gmail.com>
2026-06-29 06:21:35 +0800

    added background.sj

--- build-extension.sh
+++ build-extension.sh
@@ -7,7 +7,7 @@ rm -rf ./dist-ext
 mkdir ./dist-ext
 
 # 3. Copy your live development assets straight into the staging area
-cp index.html script.js style.css icon.png manifest.json ./dist-ext/
+cp index.html script.js style.css background.js icon.png manifest.json ./dist-ext/
 
 # 4. Create a clean flat ZIP without folder nesting using the -j (junk paths) flag
 # This extracts the files directly from the directory without nesting folders!
